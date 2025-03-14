1afeb241e4abf2a410472562376c7eb46b0fad5c
Rahul N <nrahul2727@gmail.com>
2025-03-14 21:03:54 +0530

    doc

--- polling-app/app/layout.tsx
+++ polling-app/app/layout.tsx
@@ -6,8 +6,8 @@ import Header from '@/components/Header';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Atom',
-  description: 'Learn',
+  title: 'Polling App',
+  description: 'Edraa un voter Id ah',
 }
 
 export default function RootLayout({
